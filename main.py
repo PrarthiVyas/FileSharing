90f64597a1177c02eb055e7c9e7616f22817f3ee
Prarthi Vyas <73277430+PrarthiVyas@users.noreply.github.com>
2024-12-21 21:51:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
 import table 
-from table import users, files
-from table import db
-from table import app
-from flask import Flask,request,url_for,render_template,flash,redirect
+from table import db,app,post_receipe,users, comments
+from flask import Flask,request,url_for,render_template,flash,redirect,flash,send_file
 from flask_login import LoginManager,login_user,logout_user,UserMixin,current_user,login_required
 from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,FileField,IntegerField
 from wtforms.validators import DataRequired
-from wtforms import SubmitField,FileField
-from flask_wtf.file import FileAllowed, FileRequired
-import boto3
-from botocore.exceptions import ClientError, NoCredentialsError, PartialCredentialsError
-from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import secure_filename
-from datetime import timedelta
+from flask_wtf.file import FileRequired,FileAllowed
+import io 
 
-
-
-s3 = boto3.client('s3',aws_access_key_id='Your-key',
-    aws_secret_access_key='Your-key',
-    region_name='-'
-)
 app.config['SECRET_KEY']="abc"
 login_manager=LoginManager()
 login_manager.login_view='login' # If user is not authenticated, they will be redirected here
 login_manager.init_app(app) # Connects Flask-Login to your Flask app
-class UploadForm(FlaskForm):
-    file = FileField('Upload File', validators=[
-        FileRequired(message='File is required!'),
-        FileAllowed(['jpg', 'png', 'pdf', 'txt'], 'Only images, PDFs, or text files are allowed!')
-    ])
-    submit = SubmitField('Upload')
 
+
+class addPosts(FlaskForm):
+    PT=StringField('Post Title',validators=[DataRequired()])
+    PD=StringField('Post Description',validators=[DataRequired()])
+    image=FileField("Image",validators=[FileRequired(message="This file is required"),FileAllowed(['jpg','png'],'Only Images are Allowed')])
+    submit = SubmitField("Submit")
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
@@ -57,78 +44,85 @@ def login():
         print(abc)
         if abc and abc.password==password:
             login_user(abc)
-            return redirect(url_for('filesharing'))
+            return redirect(url_for('addPost'))
         else:
             flash('Login Incorrect')
             return redirect(url_for('login'))
     return render_template('login.html')
 
 
-@app.route('/filesharing',methods=['post','get'])
+@app.route('/add_post',methods=['POST','GET'])
 @login_required
-def filesharing():
-    form=UploadForm()
-    
+def addPost():
+    form=addPosts()
+    abc=post_receipe.query.all()
+
+    if form.validate_on_submit():
+        print('hiiiii')
+        pt=form.PT.data
+        pd=form.PD.data
+        image=form.image.data
+        addpost=post_receipe(post_title=pt,post_description=pd,image=image.read(),user_id=current_user.id)
+        db.session.add(addpost)
+        db.session.commit()
+        return redirect(url_for('addPost'))
+    return render_template('post_receipe.html',form=form,data=abc)
+
+@app.route('/image/<int:id>')
+def imageProcess(id):
+    user = post_receipe.query.get(id)  # Retrieve user by ID
+    if user and user.image:
+        return send_file(
+            io.BytesIO(user.image),  # Convert binary data to a file-like object
+            mimetype='image/jpeg',  # MIME type (e.g., 'image/png' for PNG images)
+            as_attachment=False,    # Don't trigger download
+        )
+    return "Image not found", 404
+
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return users.query.get(user_id)
+
+@app.route('/get_receipe/<int:id>')
+def getReceipe(id):
+    all_comments = comments.query.filter_by(post_id=id).all()
+    getData=post_receipe.query.get(id)
+    post_likes = post_receipe.query.get(id)
+    return render_template('getData.html',data=getData, all_comments = all_comments,likes=post_likes.likes)
+
+@app.route('/add_comment/<int:id>',methods=['POST','GET'])
+def addComment(id):
 
     if request.method=="POST":
-        file = form.file.data  # Get the file from the request
-        filename = secure_filename(file.filename)
-
-        s3.upload_fileobj(file, 'aer0p1an3', filename)
-        url = s3.generate_presigned_url('get_object',
-                                                    Params={'Bucket': 'aer0p1an3',
-                                                            'Key': filename},
-                                                    ExpiresIn=60)
-
-        abc=table.files(file_name=file.filename,shared_link=url,user_id=current_user.id)
-        file_table = table.files.query.filter_by(user_id=current_user.id)
-
-        db.session.add(abc)
+        user_comment = request.form.get('comment')
+        new_comment = comments(user_id = current_user.id, post_id = id, comment = user_comment, user_name = current_user.username)
+        db.session.add(new_comment)
         db.session.commit()
-
-
-
-
-        for i in file_table:
-            print(i.file_name)
-
-        
-        return render_template('filesharing.html',form=form)
-
-
-
-    return render_template('filesharing.html',form=form)
-
-@app.route('/show_data')
-@login_required
-def show_data():
-    
-    form=UploadForm()
-    data=table.files.query.filter_by(user_id=current_user.id).all()
-    current_time=datetime.now()
-    print(current_time)
-
-    return render_template('filesharing.html',data=data,form=form,current_time=current_time)
+        return redirect(url_for('getReceipe',id=id))
+    return redirect(url_for('getReceipe',id=id))
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('login'))
-@login_manager.user_loader
-def loader_user(user_id):
-    return table.users.query.get(user_id)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    abc=table.files.query.get(id)
-    s3.delete_object(Bucket='aer0p1an3',Key= abc.file_name)
-
-    db.session.delete(abc)
+@app.route('/like/<int:id>', methods=['POST','GET'])
+@login_required
+def likePost(id):
+    post_likes = post_receipe.query.get(id)
+    post_likes_user=users.query.get(current_user.id)
+    if post_likes_user.likes == 0 and post_likes.likes==0:
+        post_likes_user.likes = 1
+        post_likes.likes = 1
+    elif post_likes_user.likes==1:
+        pass
+    elif post_likes.likes>0:
+        post_likes.likes+=1
     db.session.commit()
-    flash("Deleted Successfully")
-    return redirect(url_for('filesharing'))
-
+    return redirect(url_for('getReceipe',id=id))
     
+
 with app.app_context():
     db.create_all()
 
